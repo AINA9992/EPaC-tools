e07375cbf75badf18e5880673c8747a33a17af1f
Aqilah Syazani <aqilahsyazaniaziz@gmail.com>
2022-01-27 20:01:03 +0800

    Update passwordCracker.py

--- passwordCracker.py
+++ passwordCracker.py
@@ -13,7 +13,7 @@ def extractfile(zfile,password):
 def main():
     filename=input("Enter file name(please include extension which is .zip at the end of the file name): ")
     zfile=zipfile.ZipFile(filename)
-    passfile= open('PasswordList.zip')
+    passfile= open('PasswordList.txt')
     for line in passfile.readlines():
         password = line.strip('\n')
         guess=extractfile(zfile,password)
