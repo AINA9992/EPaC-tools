c0ac9f76af4b852a0df94bb6a01e2a31e7a37dd3
Aqilah Syazani <aqilahsyazaniaziz@gmail.com>
2022-01-30 00:57:04 +0800

    Update passwordCracker.py

--- passwordCracker.py
+++ passwordCracker.py
@@ -1,17 +1,26 @@
 from tkinter import *
 import zipfile
 import hashlib
+import random
+import string
+
+root = Tk()
+
+l_name = Label(root,text="PASSWORD CRACKER\n",font=("Arial",15))
+l_name.pack(pady=20)
 
 def extractfile(zfile,password):
     try:
         zfile.extractall(pwd=bytes(password, 'utf-8'))
         return password
     except:
-        print('cannot find the password')
+        print('Password failed')
         return
     
 def main():
-    filename=input("Enter the locked zip file name(please include extension which is .zip at the end of the file name): ")
+    filename = input("Enter the locked zip file name "
+                 "(Please include extension which is .zip"
+                 "at the end of the file name): ")
     zfile=zipfile.ZipFile(filename)
     passfile= open('PasswordList.txt')
     for line in passfile.readlines():
