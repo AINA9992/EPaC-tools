43c2286492f8de4d8413bd9161c53c6aeddb7f75
Aqilah Syazani <aqilahsyazaniaziz@gmail.com>
2022-01-30 07:54:46 +0800

    Update menu+passwordgenerator.py

--- menu+passwordgenerator.py
+++ menu+passwordgenerator.py
@@ -1,5 +1,14 @@
+import tkinter as tk
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
+import zipfile
+import hashlib
+import random
+import string
+# from tkinter import *
+# from tkinter import ttk
 from random import randint
 
 def menu():     
@@ -79,6 +88,50 @@ def menu():
         button_back.config(width=20,height=3,font=("Arial",10),bg="light blue")
         button_back.pack(padx=5)
     
+    ####################
+    # PASSWORD CRACKER #
+    ####################
+    def extractfile(zfile,password):
+        try:
+            zfile.extractall(pwd=bytes(password, 'utf-8'))
+            return password
+        except:
+            print('Wrong password')
+            return
+
+    def passwordCracker():
+        #Label frame
+        lf = LabelFrame(root,text="Enter the locked zip file name: \n"
+                                "Please include extension which is .zip"
+                                " at the end of the file name): ")
+        lf.pack(pady=20)
+            
+        # show the open file dialog
+        filetypes = (
+            ('zip files', '*.zip'),
+            ('All files', '*.*')
+        )
+            
+        filename = fd.askopenfilename( title='Open a file',
+            initialdir='/',
+            filetypes=filetypes)
+            
+        showinfo(
+            title='Selected File',
+            message=filename
+            )
+        zfile = zipfile.ZipFile(filename, 'r')
+        passfile= open('PasswordList.txt')
+        for line in passfile.readlines():
+            password = line.strip('\n')
+            guess = extractfile(zfile,password)
+            if guess:
+                print('Your password = ' + password)
+                pswd = Entry(root,text='',font=("Arial",20),bd=0,bg="systembuttonface")
+                pswd.pack(pady=20)
+                #Entry box to display output password that has been generated
+                pswd.insert(0,password)
+    
     #################
     # MENU FUNCTION #
     #################
@@ -88,7 +141,7 @@ def menu():
     l_name.pack(pady=20)
 
     #Button to go to password cracker function
-    button_pwCr = Button(root,text="Password Cracker") #,command=passwordCracker fx )
+    button_pwCr = Button(root,text="Password Cracker",command=passwordCracker)
     button_pwCr.config(width=25,height=3,font=("Arial",15),bg="light blue")
     button_pwCr.pack(pady=5)
     
